298eb0d03311569d1c874ed926926daeefce043a
SeKryGe <algerd008@gmail.com>
2022-10-26 21:43:59 +0200

    get responce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map, pluck } from 'rxjs';
+
+
+
+export interface Todo {
+  message: {
+    race: string[]
+  },
+  status: string
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Psintagram';
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
+
+  constructor (
+    private http: HttpClient
+     ) {}
+
+  ngOnInit() {
+    this.getResponse()
+  }
+
+  getResponse() {
+    return this.http.get<Todo[]>('https://dog.ceo/api/breeds/list/all', { responseType: 'json'})
+    .pipe(
+      map(obj => [...Object.values(obj)]))
+    .subscribe (todos => {
+    console.log(todos)
+    this.todos = todos
+    })
+  }
 }
